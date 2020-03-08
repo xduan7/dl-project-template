9b95c9117493195df732e8435091b0fc9842439e
Xiaotian Duan <xduan7@gmail.com>
2020-03-08 14:46:06 -0500

    add optimzier and lr scheduler type into src.optimization

--- src/optimization/__init__.py
+++ src/optimization/__init__.py
@@ -3,7 +3,9 @@ from .get_torch_optimizer import *
 
 __all__ = [
     # src.optimization.get_torch_lr_scheduler
+    'LRScheduler',
     'get_torch_lr_scheduler',
     # src.optimization.get_torch_optimizer
+    'Optimizer',
     'get_torch_optimizer',
 ]
