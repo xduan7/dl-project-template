6544e6f9cf2b51aac046c682d58fe2c8cda0ba75
Xiaotian Duan <xduan7@gmail.com>
2021-04-13 16:48:44 -0500

    fix get_computation_devices.py by breaking up the functions and other minor changes

--- src/utilities/get_computation_devices.py
+++ src/utilities/get_computation_devices.py
@@ -1,128 +1,129 @@
-"""
-File Name:          get_computation_devices.py
-Project:            dl-project-template
-
-File Description:
-
-    Get the computation devices by checking the user specification against
-    system device availability.
-
-"""
 import os
-import logging
+import warnings
 from typing import Optional, Union, List
 
 import torch
-from torch import device as Device
-try:
-    from GPUtil import getAvailable
-except (ImportError, ModuleNotFoundError):
-    getAvailable = None
+from GPUtil import getAvailable
 
 
-# criteria for GPU availability checking
+Device = Union[int, torch.device]
+
+
+# Criteria for GPU availability checking
 _MAX_NUM_GPUS = float('inf')    # max number of GPUs available
 _MAX_GPU_LOAD = 0.2             # max load to be considered as available
 _MAX_GPU_MEM_USED = 0.2         # max memory usage considered as available
 
-_LOGGER = logging.getLogger(__name__)
 
+def __get_available_gpus() -> List[Device]:
+    """Get the indices of all empty and available GPUs for PyTorch.
 
-def get_computation_devices(
-        preferred_gpu_list: Optional[Union[List[int], str]],
-        multi_gpu_flag: bool,
-) -> List[Device]:
-    """get the available computation devices (CPU & GPUs)
+    The function will first fetch the indices of all empty GPUs with
+    GPUtil by checking GPU load and memory usage. Then it modifies the
+    indices to correspond to `CUDA_VISIBLE_DEVICES`. Finally,
+    it checks with PyTorch with ``torch.cuda.get_device_properties``
+    to make sure that PyTorch has access to all the GPUs whose indices
+    are returned.
 
-    Get the computation devices for deep learning experiments with given
-    preferred list of GPU and flag for multi-GPU computation.
+    Please refer to ``__reindex_available_gpus_by_visible_devices``
+    for further details on reindexing with `CUDA_VISIBLE_DEVICES`.
 
-    :param preferred_gpu_list: preferred list of GPUs represented with
-    integers starting from 0; e.g. [0, 2] represents th first and
-    the third GPUs; none or empty list of GPUS indicate the usage of CPU;
-    and 'all' indicates all GPUs are preferred
-    :type preferred_gpu_list: Optional[Union[List[int], str]]
-    :param multi_gpu_flag: boolean flag for multi-GPU training and testing
-    :type multi_gpu_flag: bool
-    :return: list of available computation devices (CPU & GPUs) visible to
-    PyTorch (not hardware indices from PCIe)
-    :rtype: List[Device]
+    Returns: A list of GPU indices or devices that PyTorch has access to.
+
     """
-
-    # use CPU when GPUs are not preferred or not available
-    if (preferred_gpu_list is None) \
-            or (len(preferred_gpu_list) == 0) \
-            or (not torch.cuda.is_available()):
-        return [Device('cpu'), ]
-
-    # else GPUs are preferred and available
-    # get all available GPU indexes
-    _available_gpu_list: List[int]
-    if getAvailable:
-        # by default, use GPU utility package with load and memory usage
-        # specification so that the 'available' GPUs are actually ready
-        # for deep learning runs (https://github.com/anderskm/gputil)
-        _available_gpu_list = getAvailable(
-            limit=_MAX_NUM_GPUS,
-            maxLoad=_MAX_GPU_LOAD,
-            maxMemory=_MAX_GPU_MEM_USED,
-        )
-    else:
-        # assume all GPUs are good to use without GPUtil package
-        _available_gpu_list = list(range(torch.cuda.device_count()))
-        _warning_msg = \
-            f'GPUtil (https://github.com/anderskm/gputil) not installed.' \
-            f'Assuming all GPUs ({_available_gpu_list}) are available ' \
-            f'and ready for training ... '
-        _LOGGER.warning(_warning_msg)
-
-    # if CUDA_VISIBLE_DEVICES is set as an environment variable, then make
-    # sure that the list of available GPUs are visible, and re-index them in
-    # the way that PyTorch can access. For example:
-    # CUDA_VISIBLE_DEVICES = [4, 5, 6, 7]
-    # GPUs not in use = [1, 4, 7]
-    # available GPUs = [4, 7]
-    # available GPUs for PyTorch = [0, 3]
+    _available_gpus = getAvailable(
+        limit=_MAX_NUM_GPUS,
+        maxLoad=_MAX_GPU_LOAD,
+        maxMemory=_MAX_GPU_MEM_USED,
+    )
     if 'CUDA_VISIBLE_DEVICES' in os.environ:
-        _available_gpu_list_: List[int] = []
-        _cuda_visible_devices: List[int] = \
-            [int(_i) for _i in os.environ['CUDA_VISIBLE_DEVICES'].split(',')]
-        for __available_gpu_id in _available_gpu_list:
-            if __available_gpu_id in _cuda_visible_devices:
-                _available_gpu_list_.append(
-                    _cuda_visible_devices.index(__available_gpu_id))
-        _available_gpu_list = _available_gpu_list_
+        _available_gpus = \
+            __reindex_available_gpus_by_visible_devices(_available_gpus)
+    return __restrict_available_gpus_by_torch(_available_gpus)
 
-    # double check to make sure that all GPUs are accessible for PyTorch
-    _available_gpu_list_: List[int] = []
-    for __available_gpu_id in _available_gpu_list:
+
+def __reindex_available_gpus_by_visible_devices(
+        available_gpus: List[Device],
+) -> List[Device]:
+    """Re-index the list of empty GPUs with `CUDA_VISIBLE_DEVICES`.
+
+    The empty GPUs are returned from ``getAvailable``, which uses
+    Nvidia APIs directly and ignores current `CUDA_VISIBLE_DEVICES`.
+    However, PyTorch can only have access to visible devices, and the
+    indices of which are different from the global ones. This
+    function helps re-indexing the 'global' indices to the ones that
+    are applicable to PyTorch.
+
+    Example:
+        >>> # CUDA_VISIBLE_DEVICES = [2, 3]
+        >>> getAvailable(...)
+        [0, 1, 2]
+        >>> # global indices of available GPUs is [2]
+        >>> # PyTorch only has access to 2 GPUs, indexed with [0, 1]
+        >>> # indices of available GPUs after re-indexing is [0]
+        >>> __reindex_available_gpus_by_visible_devices([0, 1, 2])
+        [0]
+
+    Args:
+        available_gpus: A list of global (of all GPUs and ignores
+        `CUDA_VISIBLE_DEVICES`) indices empty GPUs.
+
+    Returns: A list of indices empty GPUs applicable to PyTorch.
+
+    """
+    _cuda_visible_devices = [
+        int(__d) for __d in
+        os.environ['CUDA_VISIBLE_DEVICES'].split(',')
+    ]
+    _reindexed_available_gpus = []
+    for __available_gpu in available_gpus:
+        if __available_gpu in _cuda_visible_devices:
+            _reindexed_available_gpus.append(
+                _cuda_visible_devices.index(__available_gpu))
+    return _reindexed_available_gpus
+
+
+def __restrict_available_gpus_by_torch(
+        available_gpus: List[Device],
+) -> List[Device]:
+    """Restrict the indices of empty and visible GPUs by checking with
+    PyTorch API for accessibility.
+
+    """
+    _restricted_available_gpus: List[Device] = []
+    for __available_gpu in available_gpus:
         try:
-            torch.cuda.get_device_properties(__available_gpu_id)
-        except Exception:
+            torch.cuda.get_device_properties(__available_gpu)
+        except AssertionError:
             _warning_msg = \
-                f'CUDA device {__available_gpu_id}, despite in the range ' \
-                f'of PyTorch GPU count, is not available.'
-            _LOGGER.warning(_warning_msg)
+                f'CUDA device {__available_gpu}, despite being ' \
+                f'available by GPUtil and visible, is not ' \
+                f'accessible by PyTorch.'
+            warnings.warn(_warning_msg)
             print(_warning_msg)
         else:
-            _available_gpu_list_.append(__available_gpu_id)
-    _available_gpu_list = _available_gpu_list_
+            _restricted_available_gpus.append(__available_gpu)
+    return _restricted_available_gpus
 
-    # get the overlap between the preferred and the available GPUs
-    if isinstance(preferred_gpu_list, str) and preferred_gpu_list == 'all':
-        _gpus = _available_gpu_list
-    elif isinstance(preferred_gpu_list, list):
-        _gpus = list(set(_available_gpu_list).intersection(preferred_gpu_list))
-    else:
-        _error_msg = \
-            f'Unknown parameter {preferred_gpu_list} for the list of ' \
-            f'preferred GPUs. Must be either \"all\" or list of integers.'
-        raise ValueError(_error_msg)
 
-    # use CPU if there is no preferred GPUs that are available
-    if len(_gpus) == 0:
-        return [Device('cpu'), ]
+def get_computation_devices() -> List[torch.device]:
+    """Get the available computation devices (CPU or GPUs) for PyTorch.
 
-    # otherwise return one or all GPUs depending on the multi-GPU flag
-    return [Device(f'cuda:{_g}') for _g in _gpus] \
-        if multi_gpu_flag else [Device(f'cuda:{_gpus[0]}'), ]
+    This function first get the global indices for all the empty GPUs,
+    with load less than ``_MAX_GPU_LOAD`` and memory usage less than
+    ``_MAX_GPU_MEM_USED``. Global indices are essentially the ones
+    from `nvidia-smi`. Then the indices of the empty GPUs will be
+    modified to make sure they are accessible by PyTorch. The
+    function will return all available GPUs in a list of
+    ``torch.device`` unless there is none, in which case the function
+    returns ``[torch.device('cpu')]``.
+
+    Returns: a list of computation devices (``torch.device``),
+        made of either  one or more GPUs or a single CPU device.
+
+    """
+    if not torch.cuda.is_available():
+        return [torch.device('cpu')]
+    _available_gpus = __get_available_gpus()
+    return [torch.device('cpu'), ] if len(_available_gpus) == 0 \
+        else [torch.device(f'cuda:{_g}') for _g in _available_gpus]
